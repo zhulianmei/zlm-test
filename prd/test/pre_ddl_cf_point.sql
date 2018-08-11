7ca293c1a70907a7b6c49ff5e919996ae4d32c3c
zlm <lm863935999@gmail.com>
2018-08-11 16:41:36 +0800

    添加新文件夹

---
+++ prd/test/pre_ddl_cf_point.sql
@@ -0,0 +1,4 @@
+alter table `cf_point`.`cf_discount_coupon` 
+add index idx_gmt_modified(gmt_modified)
+,add index idx_gmt_modified(gmt_modified);
+alter table `cf_point`.`cf_discount_coupon_deprecated_xxXX` add index idx_gmt_modified(gmt_modified);
